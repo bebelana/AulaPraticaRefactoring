1937cb0380b323f1ee6f9424cd6d4c0963cbdf24
Ana Izabel <anaizarezende@gmail.com>
2021-03-12 18:45:20 -0300

    Commit 3

--- Customer.java
+++ Customer.java
@@ -2,22 +2,22 @@ public class Customer {
    private String _name;
    private Vector _rentals = new Vector();
 
-   private double amountFor(Rental each) {
-	   //determine amounts for each line
+   private double amountFor(Rental aRental) {
+	   //determine amounts for aRental line
 	   double thisAmount = 0;
-	   switch (each.getMovie().getPriceCode()) {
+	   switch (aRental.getMovie().getPriceCode()) {
 		   case Movie.REGULAR:
 		   thisAmount += 2;
-		   if (each.getDaysRented() > 2)
-			   thisAmount += (each.getDaysRented() - 2) * 1.5;
+		   if (aRental.getDaysRented() > 2)
+			   thisAmount += (aRental.getDaysRented() - 2) * 1.5;
 		   break;
 		   case Movie.NEW_RELEASE:
-			thisAmount += each.getDaysRented() * 3;
+			thisAmount += aRental.getDaysRented() * 3;
 			break;
 		   case Movie.CHILDRENS:
 			thisAmount += 1.5;
-			if (each.getDaysRented() > 3)
-			   thisAmount += (each.getDaysRented() - 3) * 1.5;
+			if (aRental.getDaysRented() > 3)
+			   thisAmount += (aRental.getDaysRented() - 3) * 1.5;
 			break;
 		   }
 		   return thisAmount;
@@ -41,16 +41,16 @@ public class Customer {
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
         double thisAmount = 0;
-        Rental each = (Rental) rentals.nextElement();
-        thisAmount = amountFor(each);
+        Rental aRental = (Rental) rentals.nextElement();
+        thisAmount = amountFor(aRental);
 
         // add frequent renter points
         frequentRenterPoints ++;
         // add bonus for a two day new release rental
-        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            each.getDaysRented() > 1) frequentRenterPoints ++;
+        if ((aRental.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+            aRental.getDaysRented() > 1) frequentRenterPoints ++;
         //show figures for this rental
-        result += "\t" + each.getMovie().getTitle()+ "\t" +
+        result += "\t" + aRental.getMovie().getTitle()+ "\t" +
             String.valueOf(thisAmount) + "\n";
         totalAmount += thisAmount;
      }
